eaef6bf3806cac8e0cbd11fe1682f499152e8814
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-17 01:39:20 -0300

    Stream finish methods

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+L
+src/Java8LambdaRunnable.java,b\7\b7b1ea6a4c7d5a74b71f9ca7518724f4aee958be
+K
+lambda-streams-optional.iml,b\c\bc63c208fbaca52da2eb4f7abc38df34bc42ea84
+J
+src/Java8LambdaStream.java,9\f\9f6ad00129955cfbb766e818c854b18f6d565574
--- src/Java8LambdaStream.java
+++ src/Java8LambdaStream.java
@@ -1,5 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Java8LambdaStream {
     public static void main(String[] args) {
@@ -16,5 +16,31 @@ public class Java8LambdaStream {
                 .distinct()
                 .map(e -> e * 2)
                 .forEach((e) -> System.out.println(e));
+
+        long count = list.stream()
+                .limit(12)
+                .count();
+        System.out.println(count);
+
+        Optional<Integer> min = list.stream()
+                .min(Comparator.naturalOrder());
+        System.out.println(min.get());
+
+        Optional<Integer> max = list.stream()
+                .max(Comparator.naturalOrder());
+        System.out.println(max.get());
+
+        List<Integer> newList = list.stream()
+                .filter(e -> e % 2 ==0)
+                .collect(Collectors.toList());
+        System.out.println(newList);
+
+        Map<Integer, List<Integer>> collect = list.stream()
+                .collect(Collectors.groupingBy(e -> e * 3));
+        System.out.println(collect);
+
+        String s = list.stream()
+                .map(e -> String.valueOf(e))
+                .collect(Collectors.joining(";"));
     }
 }
