acf086ff1ba890a392f338d496257189966067c2
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-17 00:56:04 -0300

    Stream intermediate operations

--- src/Java8LambdaStream.java
+++ src/Java8LambdaStream.java
@@ -9,9 +9,12 @@ public class Java8LambdaStream {
 
         //stream - fluxo de dados
 
-        List<Integer> aslist = Arrays.asList(1,2,3,4,5);
-        aslist.stream()
-                .filter(e -> e % 2 == 0)
+        List<Integer> list = Arrays.asList(1,5,8,9,1,4,7,6,6,9,9);
+        list.stream()
+                .skip(2)
+                .limit(3)
+                .distinct()
+                .map(e -> e * 2)
                 .forEach((e) -> System.out.println(e));
     }
 }
